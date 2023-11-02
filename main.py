b29a0bbf3e10097afb9fcdba935266e40f42a609
chanelp <mariannissanchez27@gmail.com>
2023-11-02 18:27:49 -0400

    Parametros de ruta

--- main.py
+++ main.py
@@ -10,11 +10,63 @@ app = FastAPI()
 app.title = "First Application Programming Interface with FastApi"
 app.version = "0.0.2"
 
+data = [
+        {
+            "id": 1,
+            "title": "The Galactic Adventure",
+            "year": 2020,
+            "genre": "Sci-Fi",
+            "director": "John Director",
+            "rating": 8.0
+        },
+        {
+            "id": 2,
+            "title": "La Gran Comedia",
+            "year": 2019,
+            "genre": "Comedy",
+            "director": "Maria Director",
+            "rating": 7.5
+        },
+        {
+            "id": 3,
+            "title": "Drama in the City",
+            "year": 2021,
+            "genre": "Drama",
+            "director": "Michael Director",
+            "rating": 8.5
+        },
+        {
+            "id": 4,
+            "title": "Mystery Island",
+            "year": 2018,
+            "genre": "Mystery",
+            "director": "Emma Director",
+            "rating": 9.0
+        },
+        {
+            "id": 5,
+            "title": "Aventura Extrema",
+            "year": 2022,
+            "genre": "Adventure",
+            "director": "Daniel Director",
+            "rating": 7.8
+        }
+    ]
+
 # Creando un primer endpoint
 @app.get('/', tags = ['Home'])
 def message():
     return "Learning about fastApi"
 
+# Método GET
 @app.get('/movies', tags = ['Movies'])
 def get_movies():
-    return db.data
+    return data
+
+# Parámetros de ruta
+@app.get('/movies/{id}', tags = ["Movies"])
+def get_movie(id: int):
+    for movie in data:
+        if movie['id'] == id:
+            return movie
+    return "There is nothing to see :o"
