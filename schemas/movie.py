705efe34128c1e3d5ee5975b84dca806574e17fd
chanelp <mariannissanchez27@gmail.com>
2023-11-03 22:54:30 -0400

    Validaciones de tipos de datos: Field, model_config

--- schemas/movie.py
+++ schemas/movie.py
@@ -1,11 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 # Esquema
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    year: int
-    director: str
+    title: str = Field(min_length= 5, max_length= 15)
+    year: int = Field(le= 2023)
+    director: str = Field(min_length= 3)
     genre: str
     rating: float
+    
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                "id": 1,
+                "title": "Nombre de la peli",
+                "director": "Quien la dirige",
+                "year": 2023,
+                "genre": "Género de peli",
+                "rating": 9.0
+            }
+            ]
+        }
+   }
