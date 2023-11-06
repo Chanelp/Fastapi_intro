8d46c7dd61f97870299f15cbd85e315b803e1de4
chanelp <mariannissanchez27@gmail.com>
2023-11-06 10:03:53 -0400

    Variables de entorno

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,13 +1,19 @@
 from fastapi import Request, HTTPException
 from jwt import encode, decode
 from fastapi.security import HTTPBearer
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 def create_token(data: dict):
-    token = encode(payload= data, key= "my_secret_key", algorithm= "HS256")
+    token = encode(payload= data, key= SECRET_KEY, algorithm= "HS256")
     return token
 
 def validate_token(token: str) -> dict:
-    data: str = decode(token, key="my_secret_key", algorithms = ["HS256"])
+    data: str = decode(token, key=SECRET_KEY, algorithms = ["HS256"])
     return data
 
 # Función para pasar token a las peticiones
